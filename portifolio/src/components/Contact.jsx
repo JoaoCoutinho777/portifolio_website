db401e7aff699db351d303b8dbe7281058380abb
jac <joao.coutinho@sebasi.pt>
2023-05-24 11:59:18 +0100

    contact section designed

--- portifolio/src/components/Contact.jsx
+++ portifolio/src/components/Contact.jsx
@@ -1,15 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
+import Map from './Map';
 
 const Section = styled.div`
 height: 100vh;
-scroll-snap-align: center;
+scroll-snap-align: center; 
 `;
+const Container = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: space-between;
+gap: 50px;
+`;
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`;
+
+const Title = styled.h1`
+font-weight: 200;
+`;
+
+const Form = styled.form`
+width: 500px;
+display: flex;
+flex-direction: column;
+gap: 25px;
+`;
+
+const Input = styled.input`
+padding: 20px;
+background-color: #e8e6e6;
+border: none;
+border-radius: 5px;
+
+`;
+const TextArea = styled.textarea`
+padding: 20px;
+background-color: #e8e6e6;
+border: none;
+border-radius: 5px;
+`;
+const Button = styled.button`
+padding: 20px;
+background-color: #da4ea2;
+border: none;
+border-radius: 5px;
+font-weight: bold;
+cursor: pointer;
+color: white;
+`;
+
+const Right = styled.div`
+flex: 1;
+`;
+
 
 const Contact = () => {
     return (
         <Section>
-            Contact
+            <Container>
+                <Left>
+                    <Form>
+                        <Title>Contact Us</Title>
+                        <Input placeholder="Name"></Input>
+                        <Input placeholder="Email"></Input>
+                        <TextArea placeholder="write your message" rows={10}></TextArea>
+                        <Button>Send</Button>
+                    </Form>
+                </Left>
+                <Right>
+                    <Map />
+                </Right>
+            </Container>
         </Section>
     )
 }
