b716eb43045854a8a9e63d55c3c04ec414b34fcb
jac <joao.coutinho@sebasi.pt>
2023-05-24 11:58:11 +0100

    hero section and navbar created

--- portifolio/src/components/Navbar.jsx
+++ portifolio/src/components/Navbar.jsx
@@ -2,12 +2,81 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Section = styled.div`
-height: 100vh;
-scroll-snap-align: center;
+    display: flex;
+    justify-content: center;
 `;
 
+const Container = styled.div`
+    width: 1400px;
+    display: flex;
+    justify-content: space-between;
+    align-iems: center;
+    padding: 10px 0px;
+`;
+
+const Links = styled.div`
+    display: flex;
+    align-iems: center;
+    gap: 50px;
+`;
+
+const Icons = styled.div`
+    display: flex;
+    align-iems: center;
+    gap: 20px;
+`;
+
+const Logo = styled.img`
+    height: 50px;
+`;
+
+const List = styled.ul`
+    display: flex;
+    gap: 20px;
+    list-style: none;
+`;
+
+const ListItem = styled.li`
+    cursor: pointer
+`;
+
+const Icon = styled.img`
+    // width: 20px;
+    cursor: pointer;
+`;
+
+const Button = styled.button`
+    width: 100px;
+    padding: 10px;
+    background-color: #da4ea2;
+    color: white;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+`;
+
+
+
 export const Navbar = () => {
-  return (
-    <Section>Navbar</Section>
-  )
+    return (
+        <Section>
+            <Container>
+                <Links>
+                    <Logo src="./img/logo.png"/>
+                    <List>
+                        <ListItem>Home</ListItem>
+                        <ListItem>Studio</ListItem>
+                        <ListItem>Works</ListItem>
+                        <ListItem>Contact</ListItem>
+                    </List>
+                </Links>
+                <Icons>
+                    <Icon src="./img/search.png" />
+                    <Button>Hire Now</Button>
+                </Icons>
+            </Container> 
+        </Section>
+    )
 }
+
+export default Navbar
